1b80d55d973720de2bcfcf301f1668b2508dc1e2
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-06-23 15:30:56 -0400

    Add js database and fish objects

---
+++ scripts/database.js
@@ -0,0 +1,28 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            species: "Yellow Tang"
+            food: "crustaceans",
+            size: 3
+        },
+        {
+            name:"Lavie",
+            species: "Mandarinfish",
+            food: "Small invertibrates",
+            size: 8
+        },
+        {
+            name:"Princess Peach",
+            species:"Anthias",
+            food: "Zooplankton",
+            size: 15
+        }
+    ]
+}
